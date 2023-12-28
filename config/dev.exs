ff1171c006bdc6abdafc5b4116a7748f29a9d61b
ThanosZikas <147274171+ThanosZikas@users.noreply.github.com>
2023-12-28 10:46:33 +0000

    docker env setup

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,11 @@ import Config
 
 # Configure your database
 config :accountant, Accountant.Repo,
-  username: "postgres",
-  password: "mysecretpassword",
-  hostname: "localhost",
-  database: "accountant_dev",
+  username: System.get_env("PGUSER"),
+  password: System.get_env("PGPASSWORD"),
+  database: System.get_env("PGDATABASE"),
+  hostname: System.get_env("PGHOST"),
+  port:     System.get_env("PGPORT"),
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
